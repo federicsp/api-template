5c19cbb62d1e3912d33ae3df6f4a28350bfb8a37
Federico Spatola <federico.spatola@immobiliare.it>
2023-06-29 17:52:36 +0200

    add swagger

--- api_template/main.py
+++ api_template/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from app.routes.users import router as users_router
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
 app.include_router(users_router)
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="API-template",
+        version="0.1.0",
+        description="The project is a simple API for managing user information.",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
